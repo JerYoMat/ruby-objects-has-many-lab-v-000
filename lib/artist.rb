ce79c46bed52f44f4de14830666508ed4988a303
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-06 08:35:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,18 @@
-class Artist 
+class Artist
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
-  end 
-  
-  def add_song(song)
-    @songs << song 
-    song.artist = self 
-  end 
-  
-  def add_song_by_name()
-  end 
-  def song_count
-    @songs.count 
-  end 
-  
-  def songs 
-    @songs 
-  end 
+  end
 
-end 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  #The self keyword refers to the artist
+
+  def songs
+    @songs
+  end
+end
