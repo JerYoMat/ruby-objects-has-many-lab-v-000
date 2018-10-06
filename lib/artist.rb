0c6e7381db363b8cc9f32eb78d936e70760c5a3d
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-06 08:28:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,15 @@
 class Artist 
-  attr_accessor :name, :songs 
+  attr_accessor :name
 
   def initialize(name)
     @name = name 
+    @songs = []
   end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+  end 
+  
 
 end 
