e00b620333795d17bde8a12692c692610ae77f92
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-06 09:36:26 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,6 +13,10 @@ class Author
     post.author = self 
   end 
   
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self 
+  end 
   def posts 
     @@posts 
   end 
