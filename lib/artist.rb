65ac08d6438b98a0716257e203665e052b717c45
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-06 09:23:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,28 @@
 require 'pry'
 class Artist
-  attr_accessor :name, :songs 
+  attr_accessor :name, 
+  
+@@songs = []
   
   def initialize(name)
     @name = name
-    @songs = []
+
   end
 
   def add_song(song)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    @songs << song 
+    @@songs << song 
     song.artist = self 
    
   end 
   
   def songs
-    @songs 
+    @@songs 
   end 
   #The self keyword refers to the artist
 
